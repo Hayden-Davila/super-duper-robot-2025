88c8dfe3011f5f2faa1059c64c2d47cf0f9f084f
Hayden Davila <haydena.davila@gmail.com>
2025-05-19 21:36:10 -0500

    added files

---
+++ Card.java
@@ -0,0 +1,33 @@
+public class Card {
+    private String suit;
+    private int value;
+    private String name;
+
+    public Card(String suit, int value) {
+        this.suit = suit;
+        if(value == 1){
+            this.name = "Ace";
+        } else if(value == 11){
+            this.name = "Jack";
+        } else if(value == 12){
+            this.name = "Queen";
+        } else if(value == 13){
+            this.name = "King";
+        } else {
+            this.name = value+"";
+        }
+        this.value = value;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public int getValue() {
+        return value;
+    }
+    public String getName() {
+        return name;
+    }
+    
+}
